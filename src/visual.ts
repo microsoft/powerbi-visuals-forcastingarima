3c9096e586018db86e77fc5ef029ab0ea78b7382
boefraty74 <boefraty@microsoft.com>
2018-04-30 14:25:24 +0300

    npm upgraded

--- src/visual.ts
+++ src/visual.ts
@@ -121,11 +121,11 @@ module powerbi.extensibility.visual {
         private static parseSettings(dataView: DataView): VisualSettings {
             return VisualSettings.parse(dataView) as VisualSettings;
         }
-
+        
         /** 
         * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the 
         * objects and properties you want to expose to the users in the property pane.
-        * 
+        *
         */
         public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
             let objectName = options.objectName;
