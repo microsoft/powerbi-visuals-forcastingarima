bcfc7ba1408e4e77a0c99014527dfcf3fe3e7169
boefraty74 <boefraty@microsoft.com>
2018-04-30 16:15:19 +0300

    tslint small changes

--- src/visual.ts
+++ src/visual.ts
@@ -121,9 +121,9 @@ module powerbi.extensibility.visual {
         private static parseSettings(dataView: DataView): VisualSettings {
             return VisualSettings.parse(dataView) as VisualSettings;
         }
-        
-        /** 
-        * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the 
+
+        /**
+        * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
         * objects and properties you want to expose to the users in the property pane.
         *
         */
